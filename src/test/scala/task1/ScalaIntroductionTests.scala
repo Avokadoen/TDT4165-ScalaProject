2173c3d61679b421853b394e2adce4fea0d102bb
avokadoen <akselhallajs@gmail.com>
2020-10-27 00:15:43 +0100

    initial commit

---
+++ src/test/scala/task1/ScalaIntroductionTests.scala
@@ -0,0 +1,34 @@
+package task1
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers.convertToAnyShouldWrapper
+
+class ScalaIntroductionTests extends FunSuite {
+  val scalaIntroduction = new ScalaIntroduction
+  val arr: Array[Int] = scalaIntroduction.forLoopArray()
+
+  test("Test task 1 a: for loop that assigns array") {
+    arr.length shouldBe 50
+    arr(0) shouldBe 1
+    arr(49) shouldBe 50
+  }
+
+  test("Test task 1 b: sum of elements in array with for") {
+    val sum = scalaIntroduction.sumArrayFor(arr)
+    sum shouldBe 1275
+  }
+
+  test("Test task 1 c: sum of elements in array with recursion") {
+    val sum = scalaIntroduction.sumArrayRecurs(arr)
+    sum shouldBe 1275
+  }
+
+  test("Test task 1 d: create a fibonacci function without optimizations") {
+    scalaIntroduction.fibo(0) shouldBe 0
+    scalaIntroduction.fibo(1) shouldBe 1
+    scalaIntroduction.fibo(2) shouldBe 1
+    scalaIntroduction.fibo(3) shouldBe 2
+    scalaIntroduction.fibo(8) shouldBe 21
+    scalaIntroduction.fibo(20) shouldBe 6765
+  }
+}
