d7ac91064e7d94c2bc7059ffeb25d71ab2194adf
avokadoen <akselhallajs@gmail.com>
2020-11-05 14:49:51 +0100

    Comments

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -6,15 +6,29 @@ class Account(val bank: Bank, initialBalance: Double) {
 
     val balance = new Balance(initialBalance)
 
+    /**
+     * @param amount the amount that will be taken from the account
+     * @return Either the amount withdrawn from the account (left), or RuntimeException (rigth)
+     */
     def withdraw(amount: Double): Either[Double, RuntimeException] = balance.synchronized {
-        if(balance.amount < amount) return Right(new NoSufficientFundsException())
-        if(amount <= 0) return Right(new IllegalAmountException())
+        // if the amount in the account is insufficent
+        if (balance.amount < amount) return Right(new NoSufficientFundsException())
+
+        // if the withdraw amount is of illegal value
+        if (amount <= 0) return Right(new IllegalAmountException())
+
         balance.amount -= amount
         Left(amount)
     }
 
-    def deposit (amount: Double): Either[Double, IllegalAmountException] = balance.synchronized {
-        if(amount <= 0) return Right(new IllegalAmountException())
+    /**
+     * @param amount the desired amount to store in this account
+     * @return Either the amount we stored in the account (left), or a IllegalAmountException (right)
+     */
+    def deposit(amount: Double): Either[Double, IllegalAmountException] = balance.synchronized {
+        // if the amount is of an illegal value
+        if (amount <= 0) return Right(new IllegalAmountException())
+
         balance.amount += amount
         Left(amount)
     }
@@ -22,7 +36,7 @@ class Account(val bank: Bank, initialBalance: Double) {
     def getBalanceAmount: Double = balance.synchronized(balance.amount)
 
     def transferTo(account: Account, amount: Double): Unit = {
-        bank addTransactionToQueue (this, account, amount)
+        bank addTransactionToQueue(this, account, amount)
     }
 
 
