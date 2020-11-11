b7a22d412578c53e98b948ccb015e3c10819f369
lauri <lauritko@stud.ntnu.no>
2020-11-11 04:13:50 +0100

    Yet another version of processTransactions + few tweaks

--- src/main/scala/Bank.scala
+++ src/main/scala/Bank.scala
@@ -1,5 +1,3 @@
-import scala.annotation.tailrec
-
 class Bank(val allowedAttempts: Integer = 3) {
 
     private val transactionsQueue: TransactionQueue = new TransactionQueue()
@@ -7,7 +5,7 @@ class Bank(val allowedAttempts: Integer = 3) {
 
     /**
      * @param from   the account we are withdrawing money from
-     * @param to     the account that recieves withdrawn money
+     * @param to     the account that receives withdrawn money
      * @param amount the amount we want to transfer
      */
     def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
@@ -23,12 +21,25 @@ class Bank(val allowedAttempts: Integer = 3) {
      * Process the first transaction in the queue, if it completes it will be put in the 'processedTransactions'
      * queue, otherwise we push it in the 'transactionsQueue' and attempts to process it further
      */
-    private def processTransactions(): Unit = {
-        this.synchronized {
+    private def processTransactions(): Unit = this.synchronized {
+        /*
+        // SLOW af, but does not need sleep
+        val transaction = transactionsQueue.pop
+        val thread = Main.thread(transaction.run)
+        thread.join     // does not fail with this, and does not need the sleep in Transaction
+        // However just makes the thread obsolete anyway? Start thread, and wait for it to finish
+        if (transaction.synchronized(transaction.status == TransactionStatus.PENDING)) {
+            transactionsQueue.push(transaction)
+            processTransactions
+        }
+        else processedTransactions.push(transaction)
+         */
+
+        // less slow, but needs sleep
+        Main.thread {
             val transaction = transactionsQueue.pop
-            val thread = Main.thread(transaction.run)
-            thread.join
-            if (transaction.status == TransactionStatus.PENDING) {
+            transaction.run
+            if (transaction.synchronized(transaction.status == TransactionStatus.PENDING)) {
                 transactionsQueue.push(transaction)
                 processTransactions
             }
@@ -43,5 +54,4 @@ class Bank(val allowedAttempts: Integer = 3) {
     def getProcessedTransactionsAsList: List[Transaction] = {
         processedTransactions.iterator.toList
     }
-
 }
