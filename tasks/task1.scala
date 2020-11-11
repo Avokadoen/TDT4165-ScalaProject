059763e7e0ea3d1df158bf00a0544685e61bd46f
avokadoen <akselhallajs@gmail.com>
2020-11-11 22:20:15 +0100

    #3: Merge duplicate task1 and 2

--- tasks/task1.scala
+++ tasks/task1.scala
@@ -1,35 +1,107 @@
+import task1_2.{ScalaIntroduction, Task2}
+
+import scala.annotation.tailrec
+
 object task1 extends App {
-  // Task 1: a)
-  var arr: Array[Int] = Array()
-  for (i <- 1 to 50) arr :+= i
-  for (i <- arr) println(i)
+    def forLoopArray(): Array[Int] = {
+        val arrCount = 50
+        val arr = new Array[Int](arrCount)
+        for (i <- arr.indices) {
+            arr(i) = i + 1
+        }
 
-  var arr1 = new Array[Int](50)
-  for (i <- 1 to 50) arr1(i - 1) = i
-  for (i <- arr1) println(i)
+        arr
+    }
 
-  // b)
-  def sum(arr: Array[Int]): Int = {
-    var sum = 0
-    for (i <- arr) sum += i
-    sum
-  }
+    def sumArrayFor(arr: Array[Int]): Int = {
+        var sum = 0
+        for (member <- arr) {
+            sum += member
+        }
 
-  println(sum(arr))
+        sum
+    }
 
-  // c)
-  def sumRec(arr: Array[Int]): Int = {
-    if (arr.length > 0) arr.head + sumRec(arr.tail)
-    else 0
-  }
+    def sumArrayRecurs(arr: Array[Int]): Int = {
+        // Ensure we have defined a tail recursive function
+        @tailrec def go(arr: Array[Int], sum: Int): Int = {
+            if (arr.length > 1) {
+                // Note: I think slice here actually copies the whole thing for each call ...
+                go(arr.slice(0, arr.length - 1), sum + arr.last)
+            } else {
+                sum + arr.last
+            }
+        }
 
-  println(sumRec(arr))
+        go(arr, 0)
+    }
 
-  // d)
-  def fib(n: BigInt): BigInt = {
-    if (n <= 1) n
-    else fib(n - 1) + fib(n - 2)
-  }
+    // There is a lot difference between BigInt and Int.
+    // Int is a **primitive** 32 bit signed integer. As long as you are working
+    // with numbers between -2147483648 to 2147483647, then you should use Int.
+    // BigInt is actually a **class** as you can find the definition of BigInt:
+    // scala/package.scala:
+    // type BigInt = scala.math.BigInt
+    // BigInt.Scala:
+    // final class BigInt(val bigInteger: BigInteger)
+    // It allows for extremely big numbers at the cost of performance by combining
+    // memory locations to form one big number in contrast with Int which only takes
+    // one memory location. So BigInt is a integer with x bit size and is software implemented
+    // Int is mostly defined in the spec of most modern CPU architecture.
+    def fibo(n: Int): BigInt = {
+        if (n <= 1) {
+            return n
+        }
 
-  for (i <- 0 to 10) println(fib(i))
+        fibo(n - 1) + fibo(n - 2)
+    }
+}
+
+// Class used for testing. Included for transparancy, and if you want to hack it to run
+// this code originally had the structure of normal scala/java code where
+// src
+// | - main/scala/task1
+// | - test/scala/task1
+class ScalaIntroductionTests extends FunSuite {
+    val scalaIntroduction = new ScalaIntroduction
+    val arr: Array[Int] = scalaIntroduction.forLoopArray()
+
+    test("Test task 1 a: for loop that assigns array") {
+        arr.length shouldBe 50
+        arr(0) shouldBe 1
+        arr(49) shouldBe 50
+    }
+
+    test("Test task 1 b: sum of elements in array with for") {
+        val sum = scalaIntroduction.sumArrayFor(arr)
+        sum shouldBe 1275
+    }
+
+    test("Test task 1 c: sum of elements in array with recursion") {
+        val sum = scalaIntroduction.sumArrayRecurs(arr)
+        sum shouldBe 1275
+    }
+
+    test("Test task 1 d: create a fibonacci function without optimizations") {
+        scalaIntroduction.fibo(0) shouldBe 0
+        scalaIntroduction.fibo(1) shouldBe 1
+        scalaIntroduction.fibo(2) shouldBe 1
+        scalaIntroduction.fibo(3) shouldBe 2
+        scalaIntroduction.fibo(8) shouldBe 21
+        scalaIntroduction.fibo(20) shouldBe 6765
+    }
+
+    test("Test task 2 a: create thread with simple counter") {
+        var counter = 0
+
+        def count(): Unit = {
+            for (_ <- 1 to 1000)
+                counter += 1
+        }
+
+        val t1 = new Task2().createThread(count)
+        t1.start()
+        t1.join()
+        counter shouldBe 1000
+    }
 }
--- tasks/task2.scala
+++ tasks/task2.scala
@@ -1,58 +1,151 @@
 object task2 extends App {
-  // Task 2: a)
-  def makeThread(f: => Unit): Thread = {
-    new Thread {
-      override def run() = f
+    private var counter: Int = 0
+
+    // Task 2 a
+    def createThread(task: () => Unit): Thread = {
+        new Thread(() => task())
     }
-  }
 
-  // b)
-  private var counter: Int = 0
+    // Task 2 b
+    def increaseCounter(): Unit = {
+        // Since it was hard to actually get a discrepancy, we use a for loop her to
+        // increase the chance of the race condition occurring
+        for (_ <- 0 to 100) {
+            counter += 1
+        }
+    }
 
-  def increaseCounter(): Unit = {
-    counter += 1
-  }
+    // Task 2 c -> add 'this.synchronized' to introduce atomicity
+    // the JVM will make sure to schedule this function between threads
+    def atomIncreaseCounter(): Unit = this.synchronized {
+        for (_ <- 0 to 100) {
+            counter += 1
+        }
+    }
 
-  def printCounter(): Unit = {
-    println(counter)
-  }
+    def printCounter(): Unit = println(counter)
 
-  makeThread(increaseCounter()).start
-  makeThread(increaseCounter()).start
-  makeThread(printCounter()).start
+    // Utility function to create and start
+    def createStart(task: () => Unit): Thread = {
+        val t = createThread(task)
+        t.start()
+        t
+    }
 
-  // c)
-  def betterIncreaseCounter(): Unit = counter.synchronized {
-    counter += 1
-  }
+    // This phenomenon is called a "race condition". This happens when the program behaviour
+    // depends on OS scheduling. Race conditions are a subset of something called "undefined behaviour"
+    // meaning the programs behaviour is unpredictable. This is caused by doing things that are unpredictable
+    // according to a given language specification.
+    def runTask2B(): Unit = {
+        val ict1 = createStart(increaseCounter)
+        val ict2 = createStart(increaseCounter)
+        val pct = createStart(printCounter)
 
-  // d)
+        ict1.join()
+        ict2.join()
+        pct.join()
+    }
 
-  object A {
-    lazy val a: Int = B.b
-    lazy val a1: Int = 1
-  }
+    def runTask2C(): Unit = {
+        val ict1 = createStart(atomIncreaseCounter)
+        val ict2 = createStart(atomIncreaseCounter)
+        val pct = createThread(printCounter) // we wait with starting the thread
 
-  object B {
-    lazy val b:Int = A.a1
-  }
+        ict1.join()
+        ict2.join()
+        pct.start()
+        pct.join()
+    }
 
-  /*
-  this.synchronized {
-    val thread1 = makeThread(A.a)
-    val thread2 = makeThread(B.b)
+    // TASK 2 D
+    // This function will create a deadlock if
+    // - two threads has been assigned this function
+    // - thread 1 is called with counter 1 as c1 and counter 2 as c2
+    // - thread 2 is called with counter 2 as c1 and counter 1 as c2
 
-    thread1.start
-    thread2.start
-  }
-  */
+    // SOURCE: https://en.wikipedia.org/wiki/Deadlock#Necessary_conditions
+    // The reason the deadlock occurs is the following
+    // - At least one mutex resource is currently being used (c1)
+    // - Requesting additional resource which is being by the other thread (c2)
+    // - The resource can be released only voluntarily by the process holding it (c1 & c2)
+    // - Circular wait (thread 1 and thread 2 explained in description above)
+    def createSimpleDeadLock(): Unit = {
+        val counter1 = 0
+        val counter2 = 2
 
-  lazy val x: Int = {
-    val thread = makeThread(println(s"initializing $x"))
-    thread.start
-    thread.join
-    1
-  }
-  x
+        def workWork(c1: Int, c2: Int): Unit = {
+            c1.synchronized {
+                println(s"c1 is $c1")
+                Thread.sleep(1000)
 
+                c2.synchronized {
+                    println(s"c2 is $c2")
+                    Thread.sleep(1000)
+                }
+            }
+        }
+
+        new Thread(() => workWork(counter1, counter2)).start()
+        new Thread(() => workWork(counter2, counter1)).start()
+
+        // 1 second is more than enough for t1 and t2 theoretically,
+        // but we will not see c2 print
+        Thread.sleep(1000)
+    }
+
+    // Source: https://docs.scala-lang.org/sips/improved-lazy-val-initialization.html
+    // pretty much the same as simple only using lazy evaluation
+    object LazyDeadLock {
+        lazy val l1: Int = 1
+        lazy val l2: Int = 2
+        lazy val l3: Int = {
+            l2.synchronized {
+                Thread.sleep(1000)
+                l1.synchronized {
+                    l2 + l1
+                }
+            }
+        }
+        lazy val l4: Int = {
+            l1.synchronized {
+                Thread.sleep(1000)
+                l2.synchronized {
+                    l2 + l1
+                }
+            }
+        }
+    }
+
+    def createPossibleLazyDeadlock(): Unit = {
+        def readLazy(l: Int): Unit = this.synchronized {
+            println(l)
+        }
+
+        val t1 = new Thread(() => readLazy(LazyDeadLock.l3))
+        val t2 = new Thread(() => readLazy(LazyDeadLock.l4))
+
+        t1.start()
+        t2.start()
+
+        Thread.sleep(1000)
+    }
+
+
+    // Since task 2 is about printing, we use the runtime instead of tests to
+    // see this. Also we use a for loop to avoid having to run the program multiple times
+    for (_ <- 0 to 10) {
+        println("\nRunning loop, task 2b then task2c")
+        runTask2B()
+        runTask2C()
+    }
+
+    // Task 2 D
+    new Thread(() => createSimpleDeadLock().start()
+
+    // Do it 10 times, will *probably* meet deadlock with that many attempts
+    for (_ <- 0 to 10) {
+        new Thread(() => createPossibleLazyDeadlock().start()
+    }
+
+    println("done")
 }
